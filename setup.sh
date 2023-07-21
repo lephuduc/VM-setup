29d13879dbc3927c1b76aeb87b6498c32e94a2cf
Le Phu Duc <88520787+lephuduc@users.noreply.github.com>
2023-07-21 14:30:49 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #upgrade vm
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get upgrade  #Optional
 sudo apt install python3-pip
 
 #run 32bit
-sudo apt-get update
-sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 -y
 # install python package
 pip3 install -r requirements.txt
 
@@ -17,7 +16,7 @@ sudo snap install go -y
 
 ## insall tools
 #gdb
-sudo apt install gdb
+sudo apt install gdb -y
 #pwndbg
 git clone https://github.com/pwndbg/pwndbg
 cd pwndbg
